00f0efcb2453c57818cf9fc1ce7966d024a08b9d
Ben Strober <bstrober3@gmail.com>
2026-08-06 16:38:13 -0400

    minor change

--- sldmc_analysis/driver_key.sh
+++ sldmc_analysis/driver_key.sh
@@ -166,5 +166,5 @@ fi
 if false; then
 source ~/.bashrc
 conda activate plink_env
-fi
 Rscript visualize_sldmc_results.R ${sldmc_results_output_dir} $simulation_results_dir $borzoi_gtex_unique_target_names_file $visualize_sldmc_results_dir $annotation_name_file $simulation_oracle_results_dir
+fi
